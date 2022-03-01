a1f1dda34021a30cc80ef16e43c9cba4d731bd25
zoe.wang0505 <zoe.wang0505@gmail.com>
2022-03-01 16:44:44 -0500

    T2

--- Dev1.R
+++ Dev1.R
@@ -14,10 +14,14 @@ boxplot(data$NCLOC,data=data$NCLOC,main="NCLOC", outline = TRUE,ylab="NCLOC")
 boxplot(data$NOCom,data=data$NOCom,main="NOCom", outline = TRUE,ylab="nombre de commits")
 boxplot(data$WMC,data=data$WMC,main="WMC", outline = TRUE,ylab="complexité Weighed")
 
+#T2. Évaluer l’hypothèse selon laquelle les classes qui ont été modifiées plus de 10 fois sont mieux
+#commentées que celles qui ont été modifiées moins de 10 fois.
+data2.plus <- data[data$NOCom >= 10,]
+data2.moin <- data[data$NOCom <= 10,]
 
-
-#5.d
-library(ggplot2)
-#h5<-hist(bonds$Circuits, main="nombre de circuits par saison ",breaks = 20,ylab="circuits", xlab="Année")
-ggplot(bonds, aes(x=Année, y = Circuits )) + 
-  geom_bar(stat="identity", fill= "grey", colour= "black")
+par(mfrow=c(1,2))
+rbPal <- colorRampPalette(c('red','blue'))
+plot(data2.plus$NOCom, data2.plus$DCP, ylab = "DCP", xlab ="NoCom", main = "modifiées plus de 10 fois")
+abline(h=mean(data2.plus$DCP))
+plot(data2.moin$NOCom, data2.moin$DCP, ylab = "DCP", xlab ="NoCom", main = "modifiées moins de 10 fois")
+abline(h=mean(data2.moin$DCP))
