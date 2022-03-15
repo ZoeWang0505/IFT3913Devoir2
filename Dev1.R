2ae141745c16ea85393f38078e44a95334f40562
zoe.wang0505 <zoe.wang0505@gmail.com>
2022-03-14 22:56:24 -0400

    update for correlation and equation.

--- Dev1.R
+++ Dev1.R
@@ -2,11 +2,16 @@
 
 setwd("/home/xiaoqian/Documents/IFT3913/IFT3913Devoir2")
 
-load("/home/xiaoqian/Documents/IFT3913/IFT3913Devoir2/jfreechart-stats.csv")
+#load("/home/xiaoqian/Documents/IFT3913/IFT3913Devoir2/jfreechart-stats.csv")
+jfreechart.stats <-read.csv("/home/xiaoqian/Documents/IFT3913/IFT3913Devoir2/jfreechart-stats.csv")
 
 #T1. Diagramme à moustaches
 
 data = jfreechart.stats[,2:5]
+summary(data$DCP)
+summary(data$NCLOC)
+summary(data$NOCom)
+summary(data$WMC)
 
 par(mfrow=c(2,2))
 boxplot(data$DCP,data=data$DCP,main="DCP", outline = TRUE,ylab="densité de commentaires")
@@ -29,14 +34,17 @@ abline(h=mean(data2.moin$DCP))
 #T3 Étudier les corrélations entre NCLOC et WMC, DCP et WMC, NOCom et WMC. Visualisez les
 #données, les droits de régression, etc.
 par(mfrow=c(1,3))
+cor(data$WMC,data$NCLOC) 
 lm(data$WMC~data$NCLOC)
 plot(data$NCLOC, data$WMC, xlab = "NCLOC", ylab = "WMC", main = "corrélation entre NCLOC et WMC")
 abline(2.4864, 0.2504 )
 
+cor(data$WMC,data$DCP) 
 lm(data$WMC~data$DCP)
 plot(data$DCP, data$WMC, xlab = "DCP", ylab = "WMC", main = "corrélation entre DCP et WMC")
 abline(126.477, -1.381 )
 
+cor(data$WMC,data$NOCom)
 lm(data$WMC~data$NOCom)
 plot(data$NOCom, data$WMC, xlab = "NoCom", ylab = "WMC", main = "corrélation entre NoCom et WMC")
 abline(-20.008, 8.724 )
